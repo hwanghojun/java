b496bf22dffc3e6858fc80913005e542125d25f7
hwanghojun <pack159@naver.com>
2022-04-14 02:50:13 +0900

    [ADD]practice2

---
+++ java_code/src/org_java_code/test/test8_5.java
@@ -0,0 +1,21 @@
+package org_java_code.test;
+
+import java.util.Scanner;
+
+public class test8_5 {
+
+	public static void main(String[] args) {
+		//소문자 알파벳을 반복적으로 입력받다가 소문자 알파벳이 아니면 종료하시오.
+		Scanner HH = new Scanner(System.in);
+		
+		while(true){
+			char myStr = HH.next().charAt(0);
+			int mySecStr = (int)myStr;
+			if(mySecStr<97 || mySecStr>122) break;
+			
+		}
+		System.out.println("종료");
+		
+	}
+
+}
