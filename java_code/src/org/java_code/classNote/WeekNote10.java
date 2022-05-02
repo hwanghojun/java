15d4d4b475722e1926ec560a1f7ce439a544d2e7
hwanghojun <pack159@naver.com>
2022-05-02 20:59:29 +0900

    [ADD]method

---
+++ java_code/src/org/java_code/classNote/WeekNote10.java
@@ -0,0 +1,21 @@
+package org.java_code.classNote;
+
+public class WeekNote10 {
+
+	public static void main(String[] args) {
+		char[] ary = {'j','a','c','o','b'};
+		
+		//문자 배열을 문자열로바꾸기
+		String s =String.valueOf(ary);
+		System.out.println(s);
+		
+		//단일 문자를 문자열로 바꿀때
+		String a;
+		char c ='A';
+		s =Character.toString(c);
+		System.out.println(c+c);
+		System.out.println(s+s);
+
+	}
+
+}
---
+++ java_code/src/org_java_code/Method/Method1.java
@@ -0,0 +1,22 @@
+package org_java_code.Method;
+
+import java.util.Scanner;
+
+public class Method1 {
+	
+	public static int one() {
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		Scanner HH = new Scanner(System.in);
+		
+		int a = HH.nextInt();
+		int b = HH.nextInt();
+		
+		System.out.println();
+	}
+
+}
