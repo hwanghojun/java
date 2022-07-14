d46c0949aa925350179c6007bea3781ceaf0f903
hwanghojun <pack159@naver.com>
2022-07-14 16:47:12 +0900

    [add]classNote

---
+++ java_code/src/org/java_code/study/study04.java
@@ -0,0 +1,15 @@
+package org.java_code.study;
+
+public class study04 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
+
+// 큐 -> 앞에서 뒤로 저장하며 앞에서부터 나감 (선입선출)
+// 스택 -> 아래에서 위로 저장하며 위에서부터 나감 (후입선출)
+// push (입력) : 스택의 top자료를 삽입하는 것
+// pop(출력) : 스택의 top으로부터 원소를 제거하는 것
